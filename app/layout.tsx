1ffda62a81f902dcabd38b39384fdb8e5d1d3a65
Adam Lorek <adam.lorek@miquido.com>
2023-11-06 11:53:26 +0100

    feat(ch-16): metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "The official Next.js Learn Dashboard built with App Router.",
+  metadataBase: new URL("https://nextjs-tutorial-sooty-nine.vercel.app/"),
+};
+
 export default function RootLayout({
   children,
 }: {
